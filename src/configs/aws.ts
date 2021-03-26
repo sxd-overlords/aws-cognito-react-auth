080bbda5eb12b3cd18774899110741b7f890f864
Hervé Le Meur <herve.lemeur@sixense-group.com>
2021-03-26 11:41:35 +0100

    chore: add 'adpool' cognito pool config

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,5 +1,5 @@
 export const COGNITO = {
-  REGION: "",
-  USER_POOL_ID: "",
-  APP_CLIENT_ID: "",
+  REGION: "eu-west-3",
+  USER_POOL_ID: "eu-west-3_N15BCqvhJ",
+  APP_CLIENT_ID: "3dhplnnqvaf8p3vlpntfdgerr8",
 };
